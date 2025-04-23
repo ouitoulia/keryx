63c7f6e5f1eff93d0f563e3a43ea99aeca5abfb6
arturu <arturu@arturu.it>
2025-04-23 11:19:07 +0200

    release(1.20.1): fix target url Tipologie di procedimento

--- src/Hook/ObbligoHooks.php
+++ src/Hook/ObbligoHooks.php
@@ -414,7 +414,7 @@ class ObbligoHooks {
       $variables['content']['btn_perla_pa'] = [
         '#type' => 'markup',
         '#markup' => trim(sprintf(
-          '<p class="p-5 border w-75"><a class="btn btn-xs btn-primary my-4" href="%s" target="_blank" title="Vai ai servizi">Vai ai servizi che offre la scuola</a></p>',
+          '<p class="p-5 border w-75"><a class="btn btn-xs btn-primary my-4" href="%s" title="Vai ai servizi">Vai ai servizi che offre la scuola</a></p>',
           htmlspecialchars($url_servizi, ENT_QUOTES, 'UTF-8')
         )),
         '#weight' => 100,
